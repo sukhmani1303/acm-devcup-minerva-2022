f10ffc7778468537045db31b9eeb0c09a023fe59
YUVRAJ SINGH SUKHMANI <101919460+sukhmani1303@users.noreply.github.com>
2022-09-26 14:25:04 +0530

    using model

--- test/main.py
+++ test/main.py
@@ -1,4 +1,5 @@
 #Importing the libraries
+from copyreg import pickle
 from webbrowser import get
 from fastapi import FastAPI, Request, Body, File, UploadFile, Form
 from fastapi.responses import HTMLResponse,RedirectResponse
@@ -8,6 +9,9 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import cv2
+import pickle
+
+import keras
 
 # import multipart
 
@@ -22,7 +26,7 @@ async def home(request: Request):
     }
     return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
-def to_arr(data) -> np.ndarray:
+def rfai(data) -> np.ndarray:
     imgg = np.array(Image.open(BytesIO(data)))
     return imgg
 
@@ -30,16 +34,41 @@ def to_arr(data) -> np.ndarray:
 async def u_name(uname : UploadFile = File(...)):
 
     print("hello")
-    imh = to_arr(await uname.read())
+    imh1 = rfai(await uname.read()) #ndarray
+    imh1 = np.array(imh1)/255
+
+    # imh_new =np.expand_dims(imh, 2)
+    imh2 = cv2.resize(imh1, (150, 150))
+    # print("dims")
+    # print(imh2.shape)
+    imh_new =np.expand_dims(imh2, 2)
+    imh_new =np.expand_dims(imh_new, 0)
+    # print("dims2")
+    # print(imh_new.shape)
+    # imh = cv2.cvtColor(imh, cv2.COLOR_BGR2GRAY)
+   
     # print(type(BytesIO(await uname.read())))
+
+    # fixed_imh = cv2.cvtColor(imh, cv2.COLOR_BGR2RGB)
+    # cv2.imshow("yy",fixed_imh)
+
+    # model = pickle.load(open(r'D:\devcup-acm-2022\cnn.pkl', 'rb'))
+
+    # # score_ff = model.predict()
+
+    # predict_x = model.predict(imh_new)
+    # classes_x = np.argmax(predict_x,axis=1) 
+
+    m2 = keras.models.load_model('D:\devcup-acm-2022\cnn_model.h5')
+    yp_test = m2.predict(imh_new)
+    # yp_test = yp_test.reshape(1,-1)[0]
+    yp_test = np.argmax(yp_test, axis = 1)
+
+    # score_main = score_ff.reshape(1,-1)[0]
     
-    fixed_imh = cv2.cvtColor(imh, cv2.COLOR_BGR2RGB)
-    cv2.imshow("yy",fixed_imh)
-    
-    # waits for user to press any key
-    # (this is necessary to avoid Python kernel form crashing)
-    cv2.waitKey(0)
+    print(yp_test)
     return
+
     # response1 = RedirectResponse(url='/min')
     # return asd, image11
 
